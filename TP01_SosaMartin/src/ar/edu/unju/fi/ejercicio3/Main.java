75e7f6c05b90a87ecf38ddf8c83686a35fa64a6f
MartinSosa7 <martinchoelias7@gmail.com>
2024-04-05 01:14:46 -0300

    ejercicio 3 terminado - Sosa Martin

---
+++ TP01_SosaMartin/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		oddOrPairNumber(getNumber());
+		
+	}
+
+	static int getNumber() {
+		System.out.println("ingrese numero: ");
+		return scanner.nextInt();
+	}
+	
+	static void oddOrPairNumber(int number) {
+		if((number % 2) == 0) {
+			System.out.println("par, cuyo doble es: "+number*2);
+		}
+		else {
+			System.out.println("impar, cuyo triple doble es: "+number*3);
+		}
+	}
+}
