dc2b13697622231659b622b51040d0037e7297e0
MartinSosa7 <martinchoelias7@gmail.com>
2024-04-05 00:46:39 -0300

    ejercicio 2 terminado - Sosa Martin

---
+++ TP01_SosaMartin/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	static String countryName = "Argentina";
+	static Integer age = 20;
+	static Float buildingHeight = 50.5f;
+	static Float productPrice = 23.99f;
+	static String phoneNumber = "38812345";
+	static String cos = "0.99996192306417128873735516482698";
+	
+	public static void main(String[] args) {
+		show();
+	}
+	
+	static void show() {
+		System.out.println(countryName);
+		System.out.println(age);
+		System.out.println(buildingHeight);
+		System.out.println(productPrice);
+		System.out.println(phoneNumber);
+		System.out.println(cos);
+	}
+}
