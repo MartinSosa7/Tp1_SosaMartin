1c3945ceb6ab447df518e2425afe014568349d5e
MartinSosa7 <martinchoelias7@gmail.com>
2024-04-10 02:44:33 -0300

    ejercicio 11 terminado - Sosa Martin

---
+++ TP01_SosaMartin/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio11;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(generateSecuence());
+
+	}
+	
+	static String generateSecuence() {
+		String secuence = " ";
+		int i = 2;
+		for (int j = 40; j > 0; j--) {
+			int value = j * i;
+			secuence+=value+" ";
+			i++;
+		}
+		
+		return secuence;
+	}
+	
+
+}
